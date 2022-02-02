8a9574355e4fe01bc42097baca0eb1359acf8807
Rokas Banaitis <rokas.banaitis@gmail.com>
2022-02-02 09:20:22 +0200

    18 paskaita - JS Įvadas - kintamieji

---
+++ JavaScript/18 paskaita - JavaScript Įvadas/main.js
@@ -0,0 +1,32 @@
+console.log("Ate :) ");
+
+/*    Kintamieji
+  A) Tipai:
+    1) Skaitinis - Number | Integer/Int | Float
+      5, 54.1, -564, 2/3
+    2) Tekstinis - String | Char
+      "dvigubos", 'viengubos', `backtick`
+    3) Loginis   - Boolean | Bool
+      true, false | truthy, falsy
+
+  B) Konteineriai:
+    1) Masyvas  - Array
+      [54, false, "true", "3", "tekstas", [87, 65, true]]
+    2) Objektas - Object
+      {
+        rakto:"informacijos",
+        naujasRaktas:"naujaInfo",
+        objektoRaktas:{
+          vidinisRaktas:"vidineInfo"
+        }
+      }
+
+  C) Sukūrimo būdai:
+    1) var   - kintamasis (variable)
+      var kintamojoVardas = "Kintamasis";
+    2) let   - kintamasis
+      let kintamasis = true;
+    3) const - konstanta (constant)
+      const PI = 3.14;
+
+*/
