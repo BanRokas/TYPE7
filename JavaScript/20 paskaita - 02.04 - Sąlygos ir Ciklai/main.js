1bf0ec472cc39ef6947d69001ca72ee7773615cb
Rokas Banaitis <rokas.banaitis@gmail.com>
2022-02-04 10:17:28 +0200

    20 paskaita - Switch

---
+++ JavaScript/20 paskaita - 02.04 - Sąlygos ir Ciklai/main.js
@@ -0,0 +1,29 @@
+console.groupCollapsed("Sąlygos");
+// switch
+let vaisius = "Obuolys";
+switch(vaisius){
+  case "Obuolys":
+  case "Kriaušė":
+    console.log("Lietuviškas vaisius.");
+    break;
+  case "Dragon Fruit":
+  case "Papaya":
+  case "Kiwi":
+    console.log("Tropinis vaisius.");
+    break;
+  default:
+    console.log("Neradome");
+}
+
+/*if(vaisius == "Obuolys" || vaisius == "Kriaušė"){
+  console.log("Lietuviškas vaisius.");
+} else if(vaisius == "Dragon Fruit" || vaisius == "Papaya" || vaisius == "Kiwi"){
+  console.log("Tropinis vaisius.");
+} else {
+  console.log("Neradome");
+}*/
+console.log("-----------------");
+// ternary
+
+
+console.groupEnd();
