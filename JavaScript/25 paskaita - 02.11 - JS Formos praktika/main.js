fac2bee733835cb56d137bfa8872a547cac1fc52
Rokas Banaitis <rokas.banaitis@gmail.com>
2022-02-11 08:55:30 +0200

    25 paskaita - skaiciuotuvas1

---
+++ JavaScript/25 paskaita - 02.11 - JS Formos praktika/main.js
@@ -0,0 +1,8 @@
+/*
+  Sukurti labai paprastą skaičiuotuvą su 7 veiksmais, kuriuos renkatės iš selecto, o skaičius gaunate iš input'ų. Veiksmas vykdomas kai paspaudžiamas mygtukas. Atsakymas išvedamas ekrane šalimais arba po įvestimis.
+  input1 -> skaičius1
+  select(+,-,*,/,%,**,**(1/x)) -> veiksmai
+  input2 -> skai2ius2
+  button -> vykdymas
+  span -> atsakymas
+*/
