acbd6ea353ef040b603c364e5a5815dc18fb37ea
Rokas Banaitis <rokas.banaitis@gmail.com>
2022-02-14 13:04:46 +0200

    25 paskaita - skaiciuotuvas2 helpas1

--- JavaScript/25 paskaita - 02.11 - JS Formos praktika/main.js
+++ JavaScript/25 paskaita - 02.11 - JS Formos praktika/main.js
@@ -42,22 +42,36 @@ document.querySelector("#skaiciuok1").addEventListener("click", e => {
   }*/
 });
 
+
+//  SKAIČIUOTUVAS 2
+
 let ekranoSkaicius = null;
 let atmintiesSkaicius = null;
 let ekranoVaizdas = document.querySelector("#ekranas");
 
-
 document.querySelector("#clear").addEventListener("click", clearScreen); // CE - išvalyti tai kas parašyta ekrane
 function clearScreen(){
   ekranoSkaicius = null;
+  atvaizduoti();
 }
 document.querySelector("#reset").addEventListener("click", resetCalculator); // perkrauti skaičiuotuva
 function resetCalculator(){
   ekranoSkaicius = null;
   atmintiesSkaicius = null;
+  atvaizduoti();
 }
 document.querySelector("#zenklas").addEventListener("click", changeSign); // keisti teigiama / neigiama
+function changeSign(){
+  if(ekranoSkaicius.charAt(0) == "-"){
+    ekranoSkaicius = ekranoSkaicius.slice(1, ekranoSkaicius.length);
+  } else {
+   ekranoSkaicius = "-".concat(ekranoSkaicius);
+  }
+  atvaizduoti()
+}
+/*
 document.querySelector("#lygu").addEventListener("click", calculate); // suskaičiuoti
+
 document.querySelector("#sudetis").addEventListener("click", sudeti);
 document.querySelector("#atimtis").addEventListener("click", atimti);
 document.querySelector("#daugyba").addEventListener("click", dauginti);
@@ -65,7 +79,17 @@ document.querySelector("#dalyba").addEventListener("click", dalinti);
 document.querySelector("#liekana").addEventListener("click", gautiLiekana);
 document.querySelector("#laipsnis").addEventListener("click", pakeltiLaipsniu);
 document.querySelector("#saknis").addEventListener("click", istrauktiSakni);
+*/
 document.querySelector("#sk9").addEventListener("click", write9);
+function write9(){
+  if(ekranoSkaicius == null){
+    ekranoSkaicius = "9";
+  } else {
+    ekranoSkaicius += "9";
+  }
+  atvaizduoti();
+}
+/*
 document.querySelector("#sk8").addEventListener("click", write8);
 document.querySelector("#sk7").addEventListener("click", write7);
 document.querySelector("#sk6").addEventListener("click", write6);
@@ -75,4 +99,21 @@ document.querySelector("#sk3").addEventListener("click", write3);
 document.querySelector("#sk2").addEventListener("click", write2);
 document.querySelector("#sk1").addEventListener("click", write1);
 document.querySelector("#sk0").addEventListener("click", write0);
-document.querySelector("#skTask").addEventListener("click", writeTask);
+*/
+document.querySelector("#skTask").addEventListener("click", writeTaskas);
+function writeTaskas(){
+  if(ekranoSkaicius == null){
+    ekranoSkaicius = "0.";
+  } else {
+    if(!ekranoSkaicius.includes(".")){
+      ekranoSkaicius += ".";
+    } else if(ekranoSkaicius.charAt(ekranoSkaicius.length-1) == "."){
+      ekranoSkaicius = ekranoSkaicius.slice(0, ekranoSkaicius.length-1);
+    }
+  }
+  atvaizduoti();
+}
+
+function atvaizduoti(){
+  document.querySelector("#ekranas").innerHTML = ekranoSkaicius;
+}
