f067a7a7c6e5ebd7c7263b43c327f4967d082936
Rokas Banaitis <rokas.banaitis@gmail.com>
2022-02-17 10:51:03 +0200

    28 paskaita - teorija ir užduotis

--- JavaScript/28 paskaita - 02.17 - DOM elementų kūrimas su JS/main.js
+++ JavaScript/28 paskaita - 02.17 - DOM elementų kūrimas su JS/main.js
@@ -13,3 +13,42 @@ document.querySelector("#spalvos").addEventListener("click", e => {
   e.target.style.color = `rgb(${pirma}, ${antra}, ${trecia})`;
 });
 
+
+/*          DOM elementų kūrimas naudojant JS 
+    1) document.createElement("tagName"); - sukurti html'o elementą.
+    2) document.createTextNode("tekstas"); - sukurti teksto elementą.
+    3) parentElement.append(string|Node); - tėviniam elementui prie galo pridėti informaciją.
+    4) parentElement.appendChild(Node); - tėviniam elementui prie galo pridėti Node tipo elementą.
+    5) parentElement.prepend(string|Node); - tėviniam elementui į pradžią pridėti informaciją.
+    6) parentElement.removeChild(Node); - iš tėvinio elemento išimti nurodytą Node'ą.
+    7) element.remove(); ištrina Node elementą.
+*/
+
+const app = document.querySelector("#app");
+
+const appHeading = document.createElement("h1");
+let text = document.createTextNode("I'm a savage, classy, bougie, ratchet, sassy, moody, nasty");
+appHeading.appendChild(text);
+app.appendChild(appHeading);
+
+let p = document.createElement("p");
+let stringas = "Labas rytas";
+p.append(stringas);
+app.prepend(p);
+
+p = document.createElement("p");
+p.append("Labas vakaras");
+app.prepend(p);
+
+let removedChild = app.removeChild(p);
+let oldP = app.children[0];
+app.removeChild(oldP);
+
+let img = document.createElement("img");
+img.setAttribute("src", "images/nature5.jpg");
+img.style.height = "200px";
+app.appendChild(img);
+
+/*
+  body gale pridėti sekciją, kurioje būtų h1, img ir p
+*/
