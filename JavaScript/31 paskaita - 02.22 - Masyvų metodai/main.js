4730ff00d9e598f2f466142b839995b1a0c49f50
Rokas Banaitis <rokas.banaitis@gmail.com>
2022-02-23 11:00:49 +0200

    32 paskaita -  ForEach ir Map 1-4 uzd ats

--- JavaScript/31 paskaita - 02.22 - Masyvų metodai/main.js
+++ JavaScript/31 paskaita - 02.22 - Masyvų metodai/main.js
@@ -43,22 +43,105 @@ let findIndex6 = (mas, didMaz, ilgesnisUz, trumpesnisUz) => {
 console.log("Pirmasis žodis prasidedantis didžiąja raide ir yra ilgenis už 5, bet trumpesnis už 15 masyve yra:",findIndex6(masyvasStr, true, 5, 15),"pozicijoje.");
 console.groupEnd();
 
-/*
-  Map ir ForEach 4.1:
-  from this:
-  let data = [
-    {
-        id:0, name:"Burgeris", inStock:true, primeCost:0.3, cost:1
-    },{
-        id:1, name:"Cola", inStock:true, primeCost:0.05, cost:1.2
-    }
-  ]
-  to this:
-  let newData = [
-    {
-        id:0, name:"Burgeris", profit:0.7
-    },{
-        id:1, name:"Cola", profit:1.15
-    }
-  ]
-*/
+console.groupCollapsed("Map ir ForEach");
+
+// --- task 1 ---
+console.groupCollapsed(" --- task 1 ---");
+let mas = [
+  {id:0, name:"Cheezas", inStock:true, primeCost:0.3, cost:1},
+  {id:1, name:"HesBorger", inStock:false, primeCost:0.1, cost:2.2},
+  {id:2, name:"MacDac", inStock:true, primeCost:0.6, cost:1.6},
+  {id:3, name:"Dešrainis", inStock:false, primeCost:0.53, cost:5.4},
+  {id:4, name:"Burgeris", inStock:true, primeCost:0.34, cost:2},
+  {id:5, name:"Kebabas", inStock:false, primeCost:0.65, cost:3},
+  {id:6, name:"Cola 0.5", inStock:true, primeCost:0.1, cost:1},
+  {id:7, name:"Fanta 0.5", inStock:true, primeCost:0.1, cost:1},
+  {id:8, name:"Cola 0.2", inStock:true, primeCost:0.05, cost:0.7},
+  {id:9, name:"Fanta 0.2", inStock:true, primeCost:0.05, cost:0.7},
+  {id:10, name:"Sprite 0.2", inStock:false, primeCost:0.05, cost:0.7},
+  {id:11, name:"Sprite 0.5", inStock:false, primeCost:0.1, cost:1}
+];
+console.log(mas);
+console.groupEnd();
+
+// --- task 2 ---
+console.groupCollapsed(" --- task 2 ---");
+  mas.forEach(item => console.log(item));
+console.groupEnd();
+
+// --- task 3 ---
+console.groupCollapsed(" --- task 3 ---");
+  mas.forEach(item => console.log(`Vardas: ${item.name}; Kaina: ${item.cost}€;`));
+console.groupEnd();
+
+//    --- task 4 ---
+console.groupCollapsed(" --- task 4 ---");
+  console.groupCollapsed(" --- task 4.0 ---");
+    let mas4_0 = [];
+    mas.forEach(item => { //item.inStock ? mas4_0.push(item) : null
+      if(item.inStock === true){
+        mas4_0.push(item);
+      }
+    });
+    console.log(mas4_0);
+  console.groupEnd();
+
+  console.groupCollapsed(" --- task 4.1 ---");
+    let mas4_1 = mas4_0.map(item => ({
+      name: item.name,
+      profit: (item.cost-item.primeCost).toFixed(2)
+    }));
+    console.log(mas4_1);
+  console.groupEnd();
+
+  console.groupCollapsed(" --- task 4.2 ---");
+    let mas4_2_1 = [
+      {
+        name:"MacDac",
+        amount:4
+      }, {
+        name:"Cola 0.2",
+        amount:2
+      }
+    ];
+    let mas4_2_2 = [{name:"Burgeris", amount:2}, {name:"Fanta 0.5", amount:2}];
+    console.log(mas4_2_1);
+    console.log(mas4_2_2);
+  console.groupEnd();
+
+  console.groupCollapsed(" --- task 4.3 ---");
+    let pelnas = 0;
+    let bendriUzsakymai = mas4_2_1.concat(mas4_2_2);
+    bendriUzsakymai.forEach(uzsakymoItem => {
+      mas4_1.forEach(menuItem => {
+        if(menuItem.name === uzsakymoItem.name){
+          //console.log(menuItem.profit * uzsakymoItem.amount);
+          pelnas += menuItem.profit * uzsakymoItem.amount;
+        }
+      });
+    });
+    console.log("4.2 užduoties užsakymas/ai atnešė: " + pelnas + " pelno.");
+  console.groupEnd();
+
+  console.groupCollapsed(" --- task 4.4 ---");
+    let bendra = 0;
+    let cekis = [];
+    bendriUzsakymai.forEach( uzsakymoItem => {
+      mas4_0.forEach( menuItem =>{
+        if( menuItem.name === uzsakymoItem.name ){
+          bendra += uzsakymoItem.amount * menuItem.cost;
+          cekis.push({
+            name: uzsakymoItem.amount + " " + uzsakymoItem.name,
+            price: uzsakymoItem.amount * menuItem.cost         
+          });
+        }
+      });
+    });
+    cekis.push({bendraKaina: bendra});
+    console.log(cekis);
+  console.groupEnd();
+console.groupEnd();
+
+
+
+console.groupEnd();
