44b4dea72ec272d8592f6340d22048d33a03b8dc
Rokas Banaitis <rokas.banaitis@gmail.com>
2022-03-10 11:43:13 +0200

    43 paskaita - Fetch PUT end

--- JavaScript/43 paskaita - 03.10 - Fetch PUT/scripts/main.js
+++ JavaScript/43 paskaita - 03.10 - Fetch PUT/scripts/main.js
@@ -13,8 +13,14 @@ fetch("http://localhost:3000/movies")
       .querySelectorAll(".trintiBtn")
       .forEach(btn => {
         btn.addEventListener("click", e =>{
-          console.log(e.target.parentElement.id);
           istrintiFilma(e.target.parentElement.id);
+        })
+      })
+    document
+      .querySelectorAll(".redaguotiBtn")
+      .forEach(btn => {
+        btn.addEventListener("click", e =>{
+          pildytiRedagavimoForma(e.target.parentElement.id);
         })
       })
   })
@@ -24,7 +30,6 @@ document
   .querySelector("#addMovie")
   .addEventListener("submit", e => {
     e.preventDefault();
-    console.dir(e.target);
     let filmas = {
       pavadinimas : e.target.elements.filmoPav.value,
       paveiksliukas : e.target.elements.filmoImg.value,
@@ -32,31 +37,75 @@ document
       leidimoMetai : e.target.elements.filmoMetai.value,
       trukme : e.target.elements.filmoTrukme.value,
       IMDB : e.target.elements.filmoBalas.value,
-      tipas : [e.target.elements.filmoZanras.value],
+      tipas : e.target.elements.filmoZanras.value.split(","),
       rezisierius : e.target.elements.filmoRezisierius.value,
-      aktoriai : [e.target.elements.filmoAktorius.value]
+      aktoriai : e.target.elements.filmoAktorius.value.split(",")
     };
-    console.log(filmas);
     fetch("http://localhost:3000/movies", {
       method : "POST",
       headers : {
         "Content-type" : "application/json",
-        "Accept" : "application/json plain/text "
+        "Accept" : "application/json plain/text */*"
       },
       body : JSON.stringify(filmas)
     })
   });
+
+document
+  .querySelector("#editMovie")
+  .addEventListener("submit", e => {
+    e.preventDefault();
+    const filmoDataID = e.target.elements.submitRedagavimas.id;
+    let filmas = {
+      pavadinimas : e.target.elements.filmoPav.value,
+      paveiksliukas : e.target.elements.filmoImg.value,
+      aprasymas : e.target.elements.filmoApras.value,
+      leidimoMetai : e.target.elements.filmoMetai.value,
+      trukme : e.target.elements.filmoTrukme.value,
+      IMDB : e.target.elements.filmoBalas.value,
+      tipas : e.target.elements.filmoZanras.value.split(","),
+      rezisierius : e.target.elements.filmoRezisierius.value,
+      aktoriai : e.target.elements.filmoAktorius.value.split(",")
+    };
+
+    fetch(`http://localhost:3000/movies/${filmoDataID}`, {
+      method : "PUT",
+      headers : {
+        "Content-type" : "application/json",
+        "Accept" : "application/json plain/text */*"
+      },
+      body : JSON.stringify(filmas)
+    })
+  })
 
 let istrintiFilma = (id) => {
   fetch(`http://localhost:3000/movies/${id}`, {
     method : "DELETE"
   })
 }
+let pildytiRedagavimoForma = (id) => {
+  fetch(`http://localhost:3000/movies/${id}`)
+    .then(res => res.json())
+    .then(data => {
+      const forma = document.querySelector("#editMovie");
+      forma.elements.submitRedagavimas.id = data.id;
+      forma.elements.filmoPav.value = data.pavadinimas;
+      forma.elements.filmoImg.value = data.paveiksliukas;
+      forma.elements.filmoApras.value = data.aprasymas;
+      forma.elements.filmoMetai.value = data.leidimoMetai;
+      forma.elements.filmoTrukme.value = data.trukme;
+      forma.elements.filmoBalas.value = data.IMDB;
+      forma.elements.filmoZanras.value = data.tipas;
+      forma.elements.filmoRezisierius.value = data.rezisierius;
+      forma.elements.filmoAktorius.value = data.aktoriai;
+    })
+}
 
 let isvestiFilma = (filmas) => {
   document.querySelector("#movies").innerHTML += `
     <div class="filmas" id="${filmas.id}">
       <button class="trintiBtn">Trinti Filmą</button>
+      <button class="redaguotiBtn">Redaguoti Filmą</button>
       <h1>${filmas.pavadinimas}</h1>
       <img src="${filmas.paveiksliukas}">
       <p>${filmas.aprasymas}</p>
