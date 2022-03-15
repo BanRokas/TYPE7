883b25577fc955661c647e3dc3cf53163c23f30e
BanRokas <rokas.banaitis@gmail.com>
2022-03-15 13:23:43 +0200

    45 paskaita - Array, callback, async

--- JavaScript/45 paskaita - 03.15 - Kartojimas/scripts/main.js
+++ JavaScript/45 paskaita - 03.15 - Kartojimas/scripts/main.js
@@ -17,6 +17,7 @@ for(let i = 0; i < sakinys.length; i++){ // skirtas isvesti sakinį kas vieną s
 }
 console.groupEnd();
 
+console.groupCollapsed("Funkcijos");
 /* Funkcijos */
 function darykKazka(){
   console.log("Kažką darau...");
@@ -73,3 +74,76 @@ let funkcijosVardas1 = (par1, par2, par3) => par1*par2+par3;
 funkcijosVardas1(5,4,6); // 26
 let funkcijosVardas2 = par => par*5;
 funkcijosVardas2(10); // 50*/
+console.groupEnd();
+
+/* Array Metodai */
+
+let skaiciuMasyvas = [5,4,0,3,1,5,0,4,3,0,8,0,9,4,0,7,6,0];
+// nenaudojant iteracinių masyvų metodų būdas nr1
+let naikintiNuliusIsMasyvo = (masyvas) => {
+  for(let i = 0; i < masyvas.length; i++){
+    if(masyvas[i] === 0){
+      masyvas.splice(i, 1);
+      i--;
+    }
+  }
+  return masyvas;
+};
+console.log(naikintiNuliusIsMasyvo(skaiciuMasyvas));
+
+skaiciuMasyvas = [5,4,0,3,1,5,0,4,3,0,8,0,9,4,0,7,6,0];
+// nenaudojant iteracinių masyvų metodų būdas nr2 (kiek geresnis)
+let naikintiNuliusIsMasyvo1 = (masyvas) => {
+  let masyvasKuriGrazinti = [];
+  for(let i = 0; i < masyvas.length; i++){
+    if(masyvas[i] !== 0){
+      masyvasKuriGrazinti.push(masyvas[i]);
+    }
+  }
+  return masyvasKuriGrazinti;
+};
+console.log(naikintiNuliusIsMasyvo1(skaiciuMasyvas));
+
+// naudojant iteracinį masyvo metodą ir arrow funkciją vietoj callback
+let naikintiNuliusIsMasyvo2 = (masyvas) => {
+  return masyvas.filter(elementas => elementas !== 0);
+}
+console.log(naikintiNuliusIsMasyvo2(skaiciuMasyvas));
+
+let tikrinamArNeNulis = (skaicius) => {
+  return skaicius !== 0;
+}
+// naudojant iteracinį masyvo metodą su vienu callback
+let naikintiNuliusIsMasyvo3 = (masyvas) => {
+  return masyvas.filter(tikrinamArNeNulis);
+}
+console.log(naikintiNuliusIsMasyvo3(skaiciuMasyvas));
+
+// naudojant iteracinį masyvo metodą su keliais callback
+let naikintiNuliusIsMasyvo4 = (masyvas, callBack_as) => {
+  return masyvas.filter(callBack_as);
+}
+console.log(naikintiNuliusIsMasyvo4(skaiciuMasyvas, tikrinamArNeNulis));
+
+
+/* Async function */
+// Promise:
+function logFetchPromise(url) {
+  return fetch(url)
+    .then(response => response.text())
+    .then(text => {
+      console.log(text);
+    }).catch(err => {
+      console.error('fetch failed', err);
+    });
+}
+// Async:
+async function logFetchAsync(url) {
+  try {
+    const response = await fetch(url);
+    console.log(await response.text());
+  }
+  catch (err) {
+    console.log('fetch failed', err);
+  }
+}
