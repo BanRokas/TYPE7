9fbc33ba6246117a690ee9a2b44828b940ff43e2
BanRokas <rokas.banaitis@gmail.com>
2022-03-18 09:05:38 +0200

    48 paskaita - 5 uzduotis

---
+++ JavaScript/48 paskaita - 03.18 - Pasiruošimas JS atsiskaitymui/uzduotis5/main.js
@@ -0,0 +1,9 @@
+const asmenys = [
+  { id: '1', name: 'Petras', turiMasina: true },
+  { id: '2', name: 'Jonas', turiMasina: false },
+  { id: '3', name: 'Rokas', turiMasina: true },
+  { id: '4', name: 'Tomas', turiMasina: false },
+  { id: '5', name: 'Sima', turiMasina: true },
+  { id: '6', name: 'Kamilė', turiMasina: true },
+  { id: '7', name: 'Aloyzas', turiMasina: true }
+];
