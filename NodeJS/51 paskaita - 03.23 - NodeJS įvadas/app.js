6e5147ccc690d995f474c59edf6be87d68fc90b2
BanRokas <rokas.banaitis@gmail.com>
2022-03-23 12:02:20 +0200

    51 paskaita - NodeJS įvadas

---
+++ NodeJS/51 paskaita - 03.23 - NodeJS įvadas/app.js
@@ -0,0 +1,5 @@
+let logger = require("./logger");
+
+console.log(logger);
+
+console.log(module);
---
+++ NodeJS/51 paskaita - 03.23 - NodeJS įvadas/logger.js
@@ -0,0 +1,8 @@
+let url = "https://mylogger.io/log";
+
+function log(message){
+  console.log(message);
+}
+
+module.exports.log = log;
+module.exports.url = url;
