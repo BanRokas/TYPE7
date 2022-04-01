befa245dbd3d0433657ed7ab3f4a760d0950cb0c
BanRokas <rokas.banaitis@gmail.com>
2022-04-01 09:27:16 +0300

    57 paskaita - update IT'S ALIVE

--- NodeJS/57 paskaita - 03.31 - MongoDB sujungimas su Serveriu/index.js
+++ NodeJS/57 paskaita - 03.31 - MongoDB sujungimas su Serveriu/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const linkDataBase = "mongodb+srv://Rokas:Rokas@cluster0.vneyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const linkDataBase = `mongodb+srv://Rokas:Rokas1@cluster0.vneyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const app = express();
 
 MongoClient
   .connect(linkDataBase, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
-  .then(() => console.log("database connection succ"))
+  .then(() => console.log("database connection success"))
   .catch((err) => console.log("db error:", err));
 
 app.listen(3000, () => console.log(`Serv running on port 3000`));
