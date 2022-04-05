31b92dc5f26cae7587421bd359340e408aabd5a6
BanRokas <rokas.banaitis@gmail.com>
2022-04-05 11:52:38 +0300

    60 paskaita - HandleBars set-up

--- NodeJS/60-61 paskaita - 04.05-04.06 - Augintinių API/app.js
+++ NodeJS/60-61 paskaita - 04.05-04.06 - Augintinių API/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import router from './routes/api/augintiniai.js';
+import { engine } from 'express-handlebars';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -11,14 +12,24 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({
   extended : false
 }));
-app.use(express.static(path.resolve('public'), {
-  extensions: ['html']
-}));
+
+app.get('/', (req, res) =>{
+  res.render('home');
+});
+
 app.use('/api/augintiniai', router);
+
+// app.use(express.static(path.resolve('public'), {
+//   extensions: ['html']
+// }));
 
 app.listen(PORT, () => console.log(`Application is running on PORT ${PORT}`));
