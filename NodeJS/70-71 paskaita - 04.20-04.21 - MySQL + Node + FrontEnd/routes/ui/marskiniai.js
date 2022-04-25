3b4472d09e1afc85e4d5a41eef7aff23d9b11ec5
BanRokas <rokas.banaitis@gmail.com>
2022-04-25 15:00:52 +0300

    73 paskaita - puslapiavimas MySQL

--- NodeJS/70-71 paskaita - 04.20-04.21 - MySQL + Node + FrontEnd/routes/ui/marskiniai.js
+++ NodeJS/70-71 paskaita - 04.20-04.21 - MySQL + Node + FrontEnd/routes/ui/marskiniai.js
@@ -2,25 +2,54 @@ import express from 'express';
 import con from '../../SQL_Connect.js';
 
 const router = express.Router();
+const puslapiavimas = {
+  limit : 10,
+  skip : 0
+};
 
 router.get('/:size?', async (req, res) => {
   try {
-    const limit = req.query.limit < 10 ? 10 : 
-                  req.query.limit > 100 ? 100 : req.query.limit;
-    const [data] = await con.query(`
+    const [workingData] = await con.query(`
+      SELECT *
+      FROM shirts 
+      ${req.params.size ? `WHERE size = ?` : ''}
+    `, [req.params.size]);
+    // limitas
+    if(req.query.limit && req.query.limit !== puslapiavimas.limit){
+      puslapiavimas.limit = req.query.limit;
+      puslapiavimas.skip = 0;
+    }
+    // skipinimas
+    if(req.query.skip){
+      if(puslapiavimas.skip === 0 && Number(req.query.skip) < 0){
+      } else if(puslapiavimas.skip + Number(req.query.skip) >= workingData.length){
+      } else {
+        puslapiavimas.skip += Number(req.query.skip);
+      }
+    }
+
+    const [showData] = await con.query(`
       SELECT *
       FROM shirts
       ${req.params.size ? `WHERE size = ?` : ''}
       ORDER BY price
-      LIMIT ${limit ? limit : 10}
+      LIMIT ${puslapiavimas.limit} 
+      OFFSET ${puslapiavimas.skip}
     `, [req.params.size]);
+
+    const puslapiai = {
+      duomenuKiekis : showData.length,
+      puslapiuKiekis : Math.ceil(workingData.length / puslapiavimas.limit),
+      rodymoKiekis : puslapiavimas.limit
+    }
+
     res.render('home', {
-      marskiniai : data
+      marskiniai : showData,
+      puslapiavimas : puslapiai
     });
   } catch(e) {
     console.log(e);
-  } 
-  
+  }
 });
 
 export default router;
