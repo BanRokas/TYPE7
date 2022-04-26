6fa26920ccc19eb089fafa95cd78c6700bba2683
BanRokas <rokas.banaitis@gmail.com>
2022-04-26 08:44:19 +0300

    74 paskaita - puslapiavimas MySQL UI beautify

--- NodeJS/70-71 paskaita - 04.20-04.21 - MySQL + Node + FrontEnd/routes/ui/marskiniai.js
+++ NodeJS/70-71 paskaita - 04.20-04.21 - MySQL + Node + FrontEnd/routes/ui/marskiniai.js
@@ -10,19 +10,20 @@ const puslapiavimas = {
 router.get('/:size?', async (req, res) => {
   try {
     const [workingData] = await con.query(`
-      SELECT *
+      SELECT COUNT(*) AS rowsLength
       FROM shirts 
       ${req.params.size ? `WHERE size = ?` : ''}
     `, [req.params.size]);
+    const workingDataLength = workingData[0].rowsLength;
     // limitas
     if(req.query.limit && req.query.limit !== puslapiavimas.limit){
-      puslapiavimas.limit = req.query.limit;
+      puslapiavimas.limit = Number(req.query.limit);
       puslapiavimas.skip = 0;
     }
     // skipinimas
     if(req.query.skip){
       if(puslapiavimas.skip === 0 && Number(req.query.skip) < 0){
-      } else if(puslapiavimas.skip + Number(req.query.skip) >= workingData.length){
+      } else if(puslapiavimas.skip + Number(req.query.skip) >= workingDataLength){
       } else {
         puslapiavimas.skip += Number(req.query.skip);
       }
@@ -37,8 +38,13 @@ router.get('/:size?', async (req, res) => {
       OFFSET ${puslapiavimas.skip}
     `, [req.params.size]);
 
-    puslapiavimas.puslapiuKiekis = Math.ceil(workingData.length / puslapiavimas.limit);
+    puslapiavimas.puslapiuKiekis = Math.ceil(workingDataLength / puslapiavimas.limit);
     puslapiavimas.esamasPuslapis = puslapiavimas.skip / puslapiavimas.limit + 1;
+    puslapiavimas.pirmasPuslapis = puslapiavimas.esamasPuslapis === 1;
+    puslapiavimas.paskutinisPuslapis = puslapiavimas.puslapiuKiekis === puslapiavimas.esamasPuslapis;
+    puslapiavimas.minLimit = puslapiavimas.limit === 3;
+    puslapiavimas.medLimit = puslapiavimas.limit === 5;
+    puslapiavimas.maxLimit = puslapiavimas.limit === 10;
     
     res.render('home', {
       marskiniai : showData,
