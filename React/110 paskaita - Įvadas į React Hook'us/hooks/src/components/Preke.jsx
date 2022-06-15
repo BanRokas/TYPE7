9cc482befba40f54abbd5d4dc69906f2a5ad7e94
BanRokas <rokas.banaitis@gmail.com>
2022-06-15 10:55:02 +0300

    110 paskaita - Preke komponentas ir stiliai

--- React/110 paskaita - Įvadas į React Hook'us/hooks/src/components/Preke.jsx
+++ React/110 paskaita - Įvadas į React Hook'us/hooks/src/components/Preke.jsx
@@ -1,9 +1,12 @@
-const Preke = ({...props}) => {
-  console.log(props);
+const Preke = ({data}) => {
+  const {id, image, price, title} = data;
+  console.log(image);
   return (
-    <>
-      <div> yay prekes... {props.data.title}</div>
-    </>
+    <div className="preke">
+      <img src={image} alt={title}/>
+      <h1> {title}</h1>
+      <p> {price} </p>
+    </div>
   );
 }
  
