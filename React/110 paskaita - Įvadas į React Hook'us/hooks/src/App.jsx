f357b3a1e96ce8796244634a73398739312ae52f
BanRokas <rokas.banaitis@gmail.com>
2022-06-15 09:38:13 +0300

    110 paskaita - useState ir useEffect

--- React/110 paskaita - Įvadas į React Hook'us/hooks/src/App.jsx
+++ React/110 paskaita - Įvadas į React Hook'us/hooks/src/App.jsx
@@ -1,9 +1,35 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+
+  // let counter = 0;
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+
+  useEffect(() => {
+    console.log("Render'intas counter kintamasis.");
+  }, []);
+
+  const mygtukoNuspaudimas = () => {
+    // counter++;
+    setCounter(counter + 1);
+  }
+
+  const mygtukoNuspaudimas2 = () => {
+    setCounter2(counter2 + 1);
+  }
+
   return (
     <>
-    
+      <div>
+        <h1>Paspaudei 1 mygtuką {counter} kartų.</h1>
+        <button onClick={mygtukoNuspaudimas}>Pirmas</button>
+      </div>
+      <div>
+        <h1>Paspaudei 2 mygtuką {counter2} kartų.</h1>
+        <button onClick={mygtukoNuspaudimas2}>Antras</button>
+      </div>
     </>
   );
 }
