af9a332509725fa9b05d5b2fe5c10642ad47262f
BanRokas <rokas.banaitis@gmail.com>
2022-06-17 10:40:04 +0300

    112 paskaita - App with Routes and Nav

--- React/112 paskaita - React Router/router/src/App.jsx
+++ React/112 paskaita - React Router/router/src/App.jsx
@@ -1,9 +1,36 @@
-import './App.css';
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Nav from "./components/Nav";
+import Home from "./components/pages/Home";
+import Add from "./components/pages/Add";
 
-function App() {
+const App = () => {
+  
+  const links = [
+    {
+      title : "Home",
+      link : '/'
+    },
+    {
+      title : "Add",
+      link : '/add'
+    }
+  ];
+
   return (
     <>
-    
+      <Nav 
+        data={links}
+      />
+      <Routes>
+        <Route path="/" element={
+          <Home />
+        } />
+        <Route path="/add" element={
+          <Add />
+        } />
+      </Routes>
     </>
   );
 }
