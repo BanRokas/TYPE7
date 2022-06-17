2ff40b299f40aca498f6f3749a22ac2f7a5bd9a4
BanRokas <rokas.banaitis@gmail.com>
2022-06-17 12:03:10 +0300

    112 paskaita - Nav styles

--- React/112 paskaita - React Router/router/src/components/Nav.jsx
+++ React/112 paskaita - React Router/router/src/components/Nav.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import '../styles/Nav.css';
 
 const Nav = ({data}) => {
   return (
-    <div>
+    <div className="Navigation">
       {
         data.map((link, i) => 
           <Link
