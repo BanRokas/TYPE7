5ec68740b15b38236700c6b78c1725e22f34c228
BanRokas <rokas.banaitis@gmail.com>
2022-06-21 08:56:31 +0300

    114 paskaita - Front-end basics running

--- React/114 paskaita - 06.21 - React ir Express/client/src/App.jsx
+++ React/114 paskaita - 06.21 - React ir Express/client/src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/api')
+    .then(res => res.json())
+    .then(data => setData(data))
+  }, []);
+
   return (
-    <>
-    
-    </>
+    <div>
+      {
+        data ?
+        data.pets.map((pet, i) => 
+          <p key={i}>{pet}</p>
+        ) :
+        <p>Loading...</p>
+      }
+    </div>
   );
 }
 
